3d314447830b7b8f9721add21c34fe92ac01052a
Wilbur Zhai <s1738060@sms.ed.ac.uk>
2018-02-16 21:11:04 +0000

    muti-epoch support, try one_hot, but too comsuming

--- Train.py
+++ Train.py
@@ -3,69 +3,94 @@ import data_providers as data_providers
 import numpy as np
 import time
 
-batchSize = 5
+batchSize = 20
 train_data = data_providers.ACLIMDBDataProvider('train', batch_size=batchSize)
 valid_data = data_providers.ACLIMDBDataProvider('valid', batch_size=batchSize)
 # print(train_data.next())
 def getTrainBatch():
 	# inputs, targets = train_data.next()
-    return train_data.next()
+	try:
+		return train_data.next()
+	except:
+		train_data = data_providers.ACLIMDBDataProvider('train', batch_size=batchSize)
+		return train_data.next()
+    # return train_data.next()
 def getTestBatch():
     return valid_data.next()
 lstmUnits = 64
 numClasses = 2
-iterations = 20000//batchSize
+epoches = 50
+iterations = 20000//batchSize * epoches
 # iterations = 10
 maxSeqLength = 2505
 numDimensions = 300 #Dimensions for each word vector
+vacSize = 93928
 
 
 tf.reset_default_graph()
-# labels
-labels = tf.placeholder(tf.int32, [batchSize, numClasses], name="labels")
-# inputs
-inputs = []
-for i in range(batchSize):
-	inputs.append(tf.placeholder(tf.float32, [1,None], name="inputs_" + str(i)))
-# input_data = tf.placeholder(tf.float32, [batchSize,None], name="inputs")
+with tf.name_scope("Label"):
+	# labels
+	labels = tf.placeholder(tf.int32, [batchSize, numClasses], name="labels")
 
-# word embedding weights and bias
-W1 = tf.Variable(np.random.rand(1, numDimensions), dtype=tf.float32)
-b1 = tf.Variable(np.zeros((1,numDimensions)), dtype=tf.float32)
-# data ready for rnn input
-datas = []
-for i in range(batchSize):
-	datas.append(tf.multiply(tf.expand_dims(inputs[i], 2),tf.expand_dims(W1, 1)) + tf.expand_dims(b1, 1))
+with tf.name_scope("Input"):
+	# inputs
+	inputs = []
+	for i in range(batchSize):
+		inputs.append(tf.placeholder(tf.float32, [1,None]))
 
-# define RNN network
-lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
-lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
-values = []
-for i in range(batchSize):
-	value, _ = tf.nn.dynamic_rnn(lstmCell, datas[i], dtype=tf.float32)
-	values.append(value)
+# with tf.name_scope("One_hot"):
+# 	one_hots = []
+# 	for i in range(batchSize):
+# 		one_hots.append(tf.one_hot(inputs[i],vacSize,axis=-1))
 
-# weight and bias after RNN
-weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
-bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
+with tf.name_scope("Word2vector"):
+	# word embedding weights and bias
+	W1 = tf.Variable(np.random.rand(1, numDimensions), dtype=tf.float32, name="inputs_")
+	# one_hot version for weight
+	# W1 = tf.Variable(np.random.rand(vacSize, numDimensions), dtype=tf.float32, name="inputs_")
+	b1 = tf.Variable(np.zeros((1,numDimensions)), dtype=tf.float32, name="inputs_")
+	# data ready for rnn input
+	datas = []
+	for i in range(batchSize):
+		datas.append(tf.multiply(tf.expand_dims(inputs[i], 2),tf.expand_dims(W1, 1)) + tf.expand_dims(b1, 1))
+		# one_hot version for w2v
+		# datas.append(tf.matmul(one_hots[i],tf.expand_dims(W1,0)) + b1)
 
-# get last value from RNN output
-for i in range(batchSize):
-	values[i] = tf.transpose(values[i], [1, 0, 2])
-means = []
-for i in range(batchSize):
-	means.append(tf.reduce_mean(values[i], axis=0))
-mean_batch = tf.concat([mean for mean in means], 0)
-# get he prediction
-prediction = (tf.matmul(mean_batch, weight) + bias)
-# print(tf.shape(prediction))
-# print(tf.shape(labels))
-# calculate the accuracy
-correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
-accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+with tf.name_scope("RNN"):
+	# define RNN network
+	lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+	lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
+	values = []
+	for i in range(batchSize):
+		value, _ = tf.nn.dynamic_rnn(lstmCell, datas[i], dtype=tf.float32)
+		values.append(value)
 
-# define the loss
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
+with tf.name_scope("Mean"):
+	# get last value from RNN output
+	for i in range(batchSize):
+		values[i] = tf.transpose(values[i], [1, 0, 2])
+	means = []
+	for i in range(batchSize):
+		means.append(tf.reduce_mean(values[i], axis=0))
+
+with tf.name_scope("Concat"):
+	mean_batch = tf.concat([mean for mean in means], 0)
+
+with tf.name_scope("Prefiction"):
+	# weight and bias after RNN
+	weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]), name="inputs_")
+	bias = tf.Variable(tf.constant(0.1, shape=[numClasses]), name="inputs_")
+	# get he prediction
+	prediction = (tf.matmul(mean_batch, weight) + bias)
+
+with tf.name_scope("Acc"):
+	# calculate the accuracy
+	correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
+	accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
+with tf.name_scope("Err"):
+	# define the loss
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
 # set learning method
 optimizer = tf.train.AdamOptimizer().minimize(loss)
 
